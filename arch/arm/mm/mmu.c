b342bc2ab8bd9328ad91eecf616887a1123b3e47
Spitzbube <spitzbube@mail.bg>
2015-02-05 15:54:46 +0100

    corrected phys_mem_access_prot

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -572,7 +572,7 @@ static void __init build_mem_type_table(void)
 pgprot_t phys_mem_access_prot(struct file *file, unsigned long pfn,
 			      unsigned long size, pgprot_t vma_prot)
 {
-	if (!pfn_valid(pfn))
+	if ((!pfn_valid(pfn)) && ((pfn << 12) > 0xc0000000))
 		return pgprot_noncached(vma_prot);
 	else if (file->f_flags & O_SYNC)
 		return pgprot_writecombine(vma_prot);
